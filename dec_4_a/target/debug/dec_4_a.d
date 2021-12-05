59258f3d82655d1d2165f5c42a118c2ee9e395e0
yarocruz <yarocruz@gmail.com>
2021-12-05 18:15:25 -0500

    solved day 4 part 1

---
+++ dec_4_a/target/debug/dec_4_a.d
@@ -0,0 +1,1 @@
+/Users/yaro/Desktop/dev/AOC_2021/dec_4_a/target/debug/dec_4_a: /Users/yaro/Desktop/dev/AOC_2021/dec_4_a/src/main.rs
---
+++ dec_4_a/target/debug/deps/dec_4_a.d
@@ -0,0 +1,5 @@
+/Users/yaro/Desktop/dev/AOC_2021/dec_4_a/target/debug/deps/dec_4_a: src/main.rs
+
+/Users/yaro/Desktop/dev/AOC_2021/dec_4_a/target/debug/deps/dec_4_a.d: src/main.rs
+
+src/main.rs:
